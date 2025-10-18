3c66f3f779404e59d0ff75375e62748d431e3d47
nethoxa <nethoxa@proton.me>
2025-10-18 04:18:45 +0200

    impl deneb

---
+++ fuzztools/crates/blockchain/cl/forks/deneb.rs
@@ -0,0 +1,109 @@
+use alloy::primitives::{FixedBytes, U256};
+use super::phase0::*;
+use super::altair::*;
+use super::bellatrix::*;
+use super::capella::*;
+
+pub type VersionedHash = FixedBytes<32>;
+pub type BlobIndex = u64;
+pub type KZGCommitment = FixedBytes<48>;
+
+pub const VERSIONED_HASH_VERSION_KZG: FixedBytes<1> = FixedBytes([0x01]);
+
+pub const MAX_BLOB_COMMITMENTS_PER_BLOCK: u64 = 4096;
+pub const MAX_BLOBS_PER_BLOCK: u64 = 6;
+pub const MAX_PER_EPOCH_ACTIVATION_CHURN_LIMIT: u64 = 8;
+
+pub struct BeaconBlockBody {
+    randao_reveal: BLSSignature,
+    eth1_data: Eth1Data,
+    graffiti: FixedBytes<32>,
+    proposer_slashings: [ProposerSlashing; MAX_PROPOSER_SLASHINGS as usize],
+    attester_slashings: [AttesterSlashing; MAX_ATTESTER_SLASHINGS as usize],
+    attestations: [Attestation; MAX_ATTESTATIONS as usize],
+    deposits: [Deposit; MAX_DEPOSITS as usize],
+    voluntary_exits: [SignedVoluntaryExit; MAX_VOLUNTARY_EXITS as usize],
+    sync_aggregate: SyncAggregate,
+    // [Modified in Deneb:EIP4844]
+    execution_payload: ExecutionPayload,
+    bls_to_execution_changes: [SignedBLSToExecutionChange; MAX_BLS_TO_EXECUTION_CHANGES as usize],
+    // [New in Deneb:EIP4844]
+    blob_kzg_commitments: [KZGCommitment; MAX_BLOB_COMMITMENTS_PER_BLOCK as usize],
+}
+
+pub struct ExecutionPayload {
+    parent_hash: Hash32,
+    fee_recipient: ExecutionAddress,
+    state_root: FixedBytes<32>,
+    receipts_root: FixedBytes<32>,
+    logs_bloom: [u8; BYTES_PER_LOGS_BLOOM],
+    prev_randao: FixedBytes<32>,
+    block_number: u64,
+    gas_limit: u64,
+    gas_used: u64,
+    timestamp: u64,
+    extra_data: [u8; MAX_EXTRA_DATA_BYTES],
+    base_fee_per_gas: U256,
+    block_hash: Hash32,
+    transactions: [Transaction; MAX_TRANSACTIONS_PER_PAYLOAD as usize],
+    withdrawals: [Withdrawal; MAX_WITHDRAWALS_PER_PAYLOAD as usize],
+    // [New in Deneb:EIP4844]
+    blob_gas_used: u64,
+    // [New in Deneb:EIP4844]
+    excess_blob_gas: u64,
+}
+
+pub struct ExecutionPayloadHeader {
+    parent_hash: Hash32,
+    fee_recipient: ExecutionAddress,
+    state_root: FixedBytes<32>,
+    receipts_root: FixedBytes<32>,
+    logs_bloom: [u8; BYTES_PER_LOGS_BLOOM],
+    prev_randao: FixedBytes<32>,
+    block_number: u64,
+    gas_limit: u64,
+    gas_used: u64,
+    timestamp: u64,
+    extra_data: [u8; MAX_EXTRA_DATA_BYTES],
+    base_fee_per_gas: U256,
+    block_hash: Hash32,
+    transactions_root: Root,
+    withdrawals_root: Root,
+    // [New in Deneb:EIP4844]
+    blob_gas_used: u64,
+    // [New in Deneb:EIP4844]
+    excess_blob_gas: u64,
+}
+
+pub struct BeaconState {
+    genesis_time: u64,
+    genesis_validators_root: Root,
+    slot: Slot,
+    fork: Fork,
+    latest_block_header: BeaconBlockHeader,
+    block_roots: [Root; SLOTS_PER_HISTORICAL_ROOT as usize],
+    state_roots: [Root; SLOTS_PER_HISTORICAL_ROOT as usize],
+    historical_roots: [Root; HISTORICAL_ROOTS_LIMIT as usize],
+    eth1_data: Eth1Data,
+    eth1_data_votes: [Eth1Data; (EPOCHS_PER_ETH1_VOTING_PERIOD * SLOTS_PER_EPOCH) as usize],
+    eth1_deposit_index: u64,
+    validators: [Validator; VALIDATOR_REGISTRY_LIMIT as usize],
+    balances: [Gwei; VALIDATOR_REGISTRY_LIMIT as usize],
+    randao_mixes: [FixedBytes<32>; EPOCHS_PER_HISTORICAL_VECTOR as usize],
+    slashings: [Gwei; EPOCHS_PER_SLASHINGS_VECTOR as usize],
+    previous_epoch_participation: [ParticipationFlags; VALIDATOR_REGISTRY_LIMIT as usize],
+    current_epoch_participation: [ParticipationFlags; VALIDATOR_REGISTRY_LIMIT as usize],
+    justification_bits: [bool; JUSTIFICATION_BITS_LENGTH],
+    previous_justified_checkpoint: Checkpoint,
+    current_justified_checkpoint: Checkpoint,
+    finalized_checkpoint: Checkpoint,
+    inactivity_scores: [u64; VALIDATOR_REGISTRY_LIMIT as usize],
+    current_sync_committee: SyncCommittee,
+    next_sync_committee: SyncCommittee,
+    // [Modified in Deneb:EIP4844]
+    latest_execution_payload_header: ExecutionPayloadHeader,
+    next_withdrawal_index: WithdrawalIndex,
+    next_withdrawal_validator_index: ValidatorIndex,
+    historical_summaries: [HistoricalSummary; HISTORICAL_ROOTS_LIMIT as usize],
+}
+
